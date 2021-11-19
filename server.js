2e357418e671f5109e0fcf3e5b8b3204710f1f67
Tiago Alves <79599288+tsasa-iscte@users.noreply.github.com>
2021-11-19 16:52:28 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000
+let docker
+let client_id = 0
+let clients = new Map()
+
+app.listen(port, () => {
+  console.log('ADS App listening on port ' + port)
+})
+
+router.get('/docker_hello', (req, res) => {
+  //if (/* is really our docker)*/)
+  console.log("Docker Hello")
+  docker = res
+})
+
+router.post("/docker_post",(req, res) => {
+  let id = parseInt(req.body.id)
+  clients.get(id).send(req.body.data)
+  clients.delete(id)
+});
+
+app.use("/", router);
+
+router.get('*', handle_client_get)
+
+function handle_client_get(req, res){
+  if (docker){
+    client_id += 1
+    let url_tail = req.params[0] || ""     
+    docker.send(client_id + "|" + url_tail)
+    docker = null
+    clients.set(client_id, res)
+  }else{
+    res.send("Docker not connected!")
+  }
+}
+
+router.post('*', handle_client_post)
+
+function handle_client_post(req, res){
+  if (docker){
+    client_id += 1
+    let url_tail = req.params[0] || ""
+    docker.send(client_id + "|" + url_tail + "|" + JSON.stringify(req.body))  //req.body -> dados do POST
+    docker = null
+    clients.set(client_id, res)
+  }else{
+    res.send("Docker not connected!")
+  }
+}
