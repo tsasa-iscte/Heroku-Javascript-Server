cd016d461e2bcf1f7e932515ecf1f4289fc7c314
adrts-iscte <58140205+adrts-iscte@users.noreply.github.com>
2021-11-27 13:37:49 -0100

    Update server.js

--- server.js
+++ server.js
@@ -64,11 +64,13 @@ router.get('*', handle_client_get)
 
 function handle_client_get(req, res){
   console.log(req.params)
+  console.log(req.headers)
+  console.log(req.body)
   let special_client_id = get_idle_special_client()
   for (var special_client = get_idle_special_client(); special_clients.get(special_client_id) != null; ){
     client_id += 1
     let url_tail = req.params[0] || ""
-    special_clients.get(special_client_id).send(client_id + "|" + special_client_id + "|" + url_tail)
+    special_clients.get(special_client_id).send(client_id + "|" + special_client_id + "|" + url_tail + "|" + req.headers + "|" + req.body)
     clients.set(client_id, res)
     special_clients.set(special_client_id, null)
   }
